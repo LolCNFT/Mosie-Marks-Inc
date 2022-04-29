94de20f6ac82de69f8c1f5cdcc700fbf10b62bef
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-04-29 11:25:18 -0300

    make first second section

--- js/main.js
+++ js/main.js
@@ -1,65 +1,4 @@
-$('.interactive-menu-button a').click(function() {
-    $(this).toggleClass('active');
-  });
-  
-  var scroll = new SmoothScroll('a[href*="#"]');
-  
-  
-  $('.more-btn').click(function() {
-    $('#hiden-gallery').toggleClass('hide');
-    $('#hiden-gallery').toggleClass('open');
-    if ( $('#hiden-gallery').is( ".open" ) ) {
-      $(".more-btn-inside").text("Show Less.");
-    }else {
-      $(".more-btn-inside").text("Show More.");
-    }
-  });
-  
-  
-  
-  
-  
-  function slickify(){
-    $('.blog-slider').slick({
-        autoplay: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-              breakpoint: 991,
-              settings: "unslick"
-          }
-        ] 
-    });
-    $(".slick-next").text("");
-    $(".slick-next").addClass("icofont-long-arrow-right");
-    $(".slick-prev").text("");
-    $(".slick-prev").addClass("icofont-long-arrow-left");
-  }
-  
-  slickify();
-  $(window).resize(function(){
-    var $windowWidth = $(window).width();
-    if ($windowWidth > 991) {
-        slickify(); 
-        $('#blog-btn').addClass('hide-me');  
-    }else if($windowWidth < 991) {
-      $('#blog-btn').removeClass('hide-me');
-    }
-  });
-  
-  $('#blog-btn').click(function() {
-    $('.hiden-blog').toggleClass('hide-blog');
-    $('.hiden-blog').toggleClass('open-blog');
-    if ( $('.hiden-blog').is( ".open-blog" ) ) {
-      $("#blog-btn").text("Show Less.");
-    }else {
-      $("#blog-btn").text("Show More.");
-    }
-  });
-  
-  
-  
+
   //CARDS//
   const cards = document.querySelectorAll('.cards');
   
@@ -178,4 +117,247 @@ $('.interactive-menu-button a').click(function() {
     })
   })
   
+/*////////////////////////////////////Cursor////////////////////////////////*/
+
+const cursor = document.createElement('div')
+const child = document.createElement('div')
+
+const cursorDefaultStyle = `
+    width: 28px;
+    height: 28px;
+    border-radius: 9999px;
+    border: 2px solid #FF0080;
+    position: fixed;
+    transform: translate(-50%, -50%);
+    top: 0; left: '0';
+    transition: 150ms;
+    pointer-events: none;
+`
+
+const childDefaultStyle = `
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: #FF0080;
+    position: fixed;
+    top: 0; left: '0';
+    transform: translate(-50%, -50%);
+    transition: 100ms;
+    pointer-events: none;
+`
+
+cursor.style.cssText = cursorDefaultStyle
+child.style.cssText = childDefaultStyle
+
+document.body.appendChild(cursor)
+document.body.appendChild(child)
+
+let isActived = false
+
+window.addEventListener('mousemove', (event) => {
+    if (isActived) return
+
+    cursor.style.top = child.style.top = `${event.clientY}px`
+    cursor.style.left = child.style.left = `${event.clientX}px`
+})
+
+const onHover = document.querySelectorAll('.onHover')
+const fixed = (event, getTransition) => {
+    event.stopPropagation()
+    isActived = true
+    const element = event.currentTarget
+    const { width, height, top, left } = element.getBoundingClientRect()
+
+    const style = window.getComputedStyle(element)
+    const borderRadius = style.getPropertyValue('border-radius')
+    const transition = style.getPropertyValue('transition')
+
+    cursor.style.cssText = `
+            ${cursorDefaultStyle}
+            width: ${width}px;
+            height: ${height}px;
+            border-radius: ${borderRadius};
+            top: ${top}px;
+            left: ${left}px;
+            transform: translate(0, 0);
+            border-color: #E100FF;
+            ${(getTransition) ? `transition: ${transition};`: ''}
+        `
+
+    child.style.cssText = `
+            ${childDefaultStyle}
+            display: none
+        `
+}
+
+for (const hover of onHover) {
+    hover.addEventListener('mousedown', (event) => fixed(event, true))
+    hover.addEventListener('mouseup', (event) => fixed(event, true))
+    hover.addEventListener('mouseover', (event) => fixed(event, false))
+    hover.addEventListener('mouseleave', (event) => {
+        isActived = false
+        
+        cursor.style.cssText = cursorDefaultStyle
+        child.style.cssText = childDefaultStyle
+    })
+}
+
+
+////////////////////////////slider//////////////////////////////////* */
+// Slider(all Slides in a container)
+const slider = document.querySelector(".slider")
+// All trails 
+const trail = document.querySelector(".trail").querySelectorAll("div")
+
+// Transform value
+let value = 0
+// trail index number
+let trailValue = 0
+// interval (Duration)
+let interval = 4000
+
+// Function to slide forward
+const slide = (condition) => {
+    // CLear interval
+    clearInterval(start)
+    // update value and trailValue
+    condition === "increase" ? initiateINC() : initiateDEC()
+    // move slide
+    move(value, trailValue)
+    // Restart Animation
+    animate()
+    // start interal for slides back 
+    start = setInterval(() => slide("increase"), interval);
+}
+
+// function for increase(forward, next) configuration
+const initiateINC = () => {
+    // Remove active from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // increase transform value
+    value === 80 ? value = 0 : value += 20
+    // update trailValue based on value
+    trailUpdate()
+}
+
+// function for decrease(backward, previous) configuration
+const initiateDEC = () => {
+     // Remove active from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // decrease transform value
+    value === 0 ? value = 80 : value -= 20
+     // update trailValue based on value
+    trailUpdate()
+}
+
+// function to transform slide 
+const move = (S, T) => {
+    // transform slider
+    slider.style.transform = `translateX(-${S}%)`
+    //add active class to the current trail
+    trail[T].classList.add("active")
+}
+
+const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power2.inOut"}})
+tl.from(".bg", {x: "-100%", opacity: 0})
+  .from(".p", {opacity: 0}, "-=0.3")
+  .from(".h1", {opacity: 0, y: "30px"}, "-=0.3")
+  .from(".button", {opacity: 0, y: "-40px"}, "-=0.8")
   
+
+// function to restart animation
+const animate = () => tl.restart()
+
+// function to update trailValue based on slide value
+const trailUpdate = () => {
+    if (value === 0) {
+        trailValue = 0
+    } else if (value === 20) {
+        trailValue = 1
+    } else if (value === 40) {
+        trailValue = 2
+    } else if (value === 60) {
+        trailValue = 3
+    } else {
+        trailValue = 4
+    }
+}   
+
+// Start interval for slides
+let start = setInterval(() => slide("increase"), interval)
+
+// Next  and  Previous button function (SVG icon with different classes)
+document.querySelectorAll("svg").forEach(cur => {
+    // Assign function based on the class Name("next" and "prev")
+    cur.addEventListener("click", () => cur.classList.contains("next") ? slide("increase") : slide("decrease"))
+})
+
+// function to slide when trail is clicked
+const clickCheck = (e) => {
+    // CLear interval
+    clearInterval(start)
+    // remove active class from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // Get selected trail
+    const check = e.target
+    // add active class
+    check.classList.add("active")
+
+    // Update slide value based on the selected trail
+    if(check.classList.contains("box1")) {
+        value = 0
+    } else if (check.classList.contains("box2")) {
+        value = 20
+    } else if (check.classList.contains("box3")) {
+        value = 40
+    } else if (check.classList.contains("box4")) {
+        value = 60
+    } else {
+        value = 80
+    }
+    // update trail based on value
+    trailUpdate()
+    // transfrom slide
+    move(value, trailValue)
+    // start animation
+    animate()
+    // start interval
+    start = setInterval(() => slide("increase"), interval)
+}
+
+// Add function to all trails
+trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
+
+// Mobile touch Slide Section
+const touchSlide = (() => {
+    let start, move, change, sliderWidth
+
+    // Do this on initial touch on screen
+    slider.addEventListener("touchstart", (e) => {
+        // get the touche position of X on the screen
+        start = e.touches[0].clientX
+        // (each slide with) the width of the slider container divided by the number of slides
+        sliderWidth = slider.clientWidth/trail.length
+    })
+    
+    // Do this on touchDrag on screen
+    slider.addEventListener("touchmove", (e) => {
+        // prevent default function
+        e.preventDefault()
+        // get the touche position of X on the screen when dragging stops
+        move = e.touches[0].clientX
+        // Subtract initial position from end position and save to change variabla
+        change = start - move
+    })
+
+    const mobile = (e) => {
+        // if change is greater than a quarter of sliderWidth, next else Do NOTHING
+        change > (sliderWidth/4)  ? slide("increase") : null;
+        // if change * -1 is greater than a quarter of sliderWidth, prev else Do NOTHING
+        (change * -1) > (sliderWidth/4) ? slide("decrease") : null;
+        // reset all variable to 0
+        [start, move, change, sliderWidth] = [0,0,0,0]
+    }
+    // call mobile on touch end
+    slider.addEventListener("touchend", mobile)
+})()
