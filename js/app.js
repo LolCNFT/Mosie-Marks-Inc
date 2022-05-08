1f0a2232d6210dbc891e0ade33c97ba2595a8811
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-05-08 20:48:42 -0300

    corrections on window.load

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
 import * as S from "./@emurgo/cardano-serialization-lib-asmjs/cardano_serialization_lib.js"
 import * as B from './buffer/index.js'
+
 const Buffer = B.Buffer
 
 let walletclient
 
 window.onload = function() {
-  const wallets = ["nami", "eternl"];
-  var namiconnected = localStorage.getItem(namiconnected);
+  const wallets = ["Nami", "Eternl"];
   var someVarName = localStorage.getItem("someVarKey");
   if (someVarName === 'nami') {
     console.log(someVarName);
+    var someVarName = "";
+    localStorage.setItem("someVarKey", someVarName);
     connectNami();
   }
   if (someVarName === 'eternl') {
     console.log(someVarName);
+    var someVarName = "";
+    localStorage.setItem("someVarKey", someVarName);
     connectEternl();
   };
   for (const wallet of wallets) {
     if (window.cardano[wallet]?.isEnabled()) {
-      console.log(`${wallet} wallet is enabled`);
+      console.log(`${wallet} Wallet is enabled`);
     } else {
-      console.log(`${wallet} wallet is not enabled`);
+      console.log(`${wallet} Wallet is not enabled`);
     };
   };
 
@@ -38,6 +42,7 @@ var img = document.createElement("img");
 
 
 async function connectNami() {
+
   if (window.cardano.nami) {
     try {
       walletclient = await window.cardano.nami.enable();
@@ -51,12 +56,16 @@ async function connectNami() {
           const address = S.Address.from_bytes(Buffer.from(addressHex, "hex")).to_bech32();
           const walletstring = `${address.substring(0, 5)}***${address.substring(address.length - 5)}`;
           console.log(address);
+
           document.getElementById('wallet').innerText = walletstring;
           var logoWallet = document.getElementById('wallet-icon-selected');
           logoWallet.appendChild(img);
           //onload autoconnect
           var someVarName = "nami";
           localStorage.setItem("someVarKey", someVarName);
+
+          // const amount = (await walletclient.getBalance());
+          // console.log(amount)
         }
         else {
           window.alert("You're on testnet");
@@ -90,6 +99,9 @@ async function connectEternl(){
           //onload autoconnect
           var someVarName = "eternl";
           localStorage.setItem("someVarKey", someVarName);
+
+          // const balance = (await walletclient.getBalance());
+          // console.log(balance)
         }
         else {
           window.alert("You're on testnet");
@@ -100,3 +112,23 @@ async function connectEternl(){
     };
   }
 };
+
+
+  //   /**
+  //    * Gets the current balance of in Lovelace in the user's wallet
+  //    * This doesnt resturn the amounts of all other Tokens
+  //    * For other tokens you need to look into the full UTXO list
+  //    * @returns {Promise<void>}
+  //    */
+  //    getBalance = async () => {
+  //     try {
+  //         const balanceCBORHex = await this.API.getBalance();
+
+  //         const balance = Value.from_bytes(Buffer.from(balanceCBORHex, "hex")).coin().to_str();
+  //         this.setState({balance})
+  //         console.log(balance)
+
+  //     } catch (err) {
+  //         console.log(err)
+  //     }
+  // }
